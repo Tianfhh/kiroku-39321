d5bb1e10fb011a902e25aed0254b386cc2e21b1d
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-27 19:32:28 +0900

    Update 20230827001523_create_entries.rb

--- db/migrate/20230827001523_create_entries.rb
+++ db/migrate/20230827001523_create_entries.rb
@@ -2,7 +2,6 @@ class CreateEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :entries do |t|
 
-      t.text :photo
       t.string :title, null: false
       t.text :text, null: false
       
